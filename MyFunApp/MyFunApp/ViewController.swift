87b22a8b4b1dab772b1dc146b497bdc3b4130899
Mikey Ward <mw@mikey.io>
2020-10-15 09:03:35 -0400

    Add initial, empty Xcode project

---
+++ MyFunApp/MyFunApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFunApp
+//
+//  Created by Mikey Ward on 10/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
