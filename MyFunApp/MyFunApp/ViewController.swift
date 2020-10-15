c89461cb4be1c72e332f72cb0a6796dee3a20d9b
Mikey Ward <mw@mikey.io>
2020-10-15 09:07:19 -0400

    Add simple UI button and its action method

--- MyFunApp/MyFunApp/ViewController.swift
+++ MyFunApp/MyFunApp/ViewController.swift
@@ -9,9 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func handleTappedButton(_ sender: UIButton) {
+        print("tip tap")
     }
 
 
